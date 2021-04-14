9ad4c43748b4e3d55d73e9c6afcb5468b898c7d2
Marta Sleboda <sleboda.m98@gmail.com>
2021-04-14 22:58:34 +0200

    more methods

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.get("/method")
+@app.get("/")
 def read_root():
+    return {"message": "Hello world!"}
+
+@app.get("/method")
+def read_item():
     return {"method": "GET"}
+
+@app.put("/method")
+def put_item():
+    return {"method": "POST"}
+
+@app.options()
+def met_info():
+    return {"method": "OPTIONS"}
+
+@app.delete()
+def deleting_stuff():
+    return {"method": "DELETE"}
+
+@app.post("/method")
+def post_item():
+    return {"method": "POST"}
